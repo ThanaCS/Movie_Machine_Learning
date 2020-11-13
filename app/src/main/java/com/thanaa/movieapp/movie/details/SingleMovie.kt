951ad19c5c13404de66127e41c74f83e797c2684
ThanaCS <Thana_cs@hotmail.com>
2020-11-13 20:48:37 +0300

    Initial commit

---
+++ app/src/main/java/com/thanaa/movieapp/movie/details/SingleMovie.kt
@@ -0,0 +1,12 @@
+package com.thanaa.movieapp.movie.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.thanaa.movieapp.R
+
+class SingleMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signle_movie)
+    }
+}
